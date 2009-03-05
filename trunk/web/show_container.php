d42e4b2e405e63f1e7b6cbc7e5e6988ff66f9b6e
mauser <mauser@3d16c93d-e620-0410-bbdf-cfbb2802f564>
2009-03-05 15:07:01 +0000

    code reformating
    git-svn-id: http://svn6.assembla.com/svn/datenfresser@73 3d16c93d-e620-0410-bbdf-cfbb2802f564

--- trunk/web/show_container.php
+++ trunk/web/show_container.php
@@ -5,24 +5,18 @@ include("datenfresser_core.php");
 $core = new datenfresser(); 
 $core->print_header();
 
+
 if($_GET["id"] != ""){
-
-
-
-$dbh = new PDO('sqlite:/var/datenfresser/datenfresser.db');
-
-print "<br /><table>\n";
-
-$data = $dbh->query("SELECT dataID,name,remotePath,comment,schedule,type,options FROM dataContainer WHERE dataID ='".$_GET["id"]."'");
-$d = $data->fetchAll(PDO::FETCH_ASSOC);
-foreach ($d as $row) {
-
-	foreach( $row as $key => $value ){
-		print "<tr><td>$key:</td><td>".$value."</td>";
+	$dbh = new PDO('sqlite:/var/datenfresser/datenfresser.db');
+	print "<br /><table>\n";
+	$data = $dbh->query("SELECT dataID,name,remotePath,comment,schedule,type,options FROM dataContainer WHERE dataID ='".$_GET["id"]."'");
+	$d = $data->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($d as $row) {
+		foreach( $row as $key => $value ){
+			print "<tr><td>$key:</td><td>".$value."</td>";
+		}	
 	}	
 }
 
-	
-}
 
 ?>
