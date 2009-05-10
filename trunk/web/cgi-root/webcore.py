21ecb74d39f4fccfef3078a7ddf98b545891b043
mauser <mauser@3d16c93d-e620-0410-bbdf-cfbb2802f564>
2009-05-10 21:12:06 +0000

    added error-method to web interface
    git-svn-id: http://svn6.assembla.com/svn/datenfresser@103 3d16c93d-e620-0410-bbdf-cfbb2802f564

--- trunk/web/cgi-root/webcore.py
+++ trunk/web/cgi-root/webcore.py
@@ -6,3 +6,6 @@ class datenfresser_web:
 		print
 	    	print "<html><head><title>Datenfresser</title><link rel='stylesheet' type='text/css' href='datenfresser.css'></head><body>" ;
 		print " <div id='banner'><img src='images/small.png'><div id='banner_text'><a href='index.py'>Show container</a>&nbsp;<a href='show_logs.py'>Show logs</a>&nbsp;<a href='add_container.py'>Add container</a>&nbsp;</div></div><div height='300'>&nbsp;<br /><br /><br /><br /><br /></div>";
+    
+    def print_error( self , error_message ):
+		print "<div id='error'>" + error_message + "</div>";
