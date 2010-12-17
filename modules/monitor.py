e1d8dde4071c7c824754cf6fc5fa84b03295df79
Sebastian Moors <mauser@smoors.de>
2010-12-17 22:55:09 +0100

    added simple authentication for the monitoring server

--- modules/monitor.py
+++ modules/monitor.py
@@ -23,34 +23,73 @@ class datenfresserMonitorServer:
 	    self.startServer()
 
     def startServer( self ):
-	#TODO: move log to datenfresserCommon	
-	#print "Starting datenfresser monitoring server on port %s" % self.__listen_port
+	print "Starting datenfresser monitoring server on port %s" % self.port
 	
 	#or logEntry in self.dataBase.getLogs(0):
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	server.bind(("", 50000)) 
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	
+	server.bind(("", int(self.port))) 
 	server.listen(1)
 
 	clients = []
+	state = {}
 
 	try: 
 	    while True: 
 		read, write, oob = select.select([server] + clients, [], [])
 
 		for sock in read: 
-		    if sock in server: 
+		    if sock is server: 
 			client, addr = server.accept() 
 			clients.append(client) 
 			print "#Client %s connected" % addr[0] 
 		    else: 
-			nachricht = sock.recv(1024) 
-			ip = sock.getpeername()[0] 
-			if nachricht: 
-			    print "[%s] %s" % (ip, nachricht) 
+			message = sock.recv(1024) 
+			ip = sock.getpeername()[0]
+			ipPortTuple = sock.getpeername()
+			
+
+			#every client has to authenticate itself at the
+			#beginning of a transmission
+			if message[0:4] == "auth":
+				parts = message.split(" ")
+				user = parts[1].strip()
+				password = parts[2].strip()
+
+				if user == "mauser" and password == "test":
+					state[ ipPortTuple ] = "ok"
+				else:
+					print "wrong credentials"
+					print user
+					print password
+					
+	
+			    	print "[%s] %s" % (ip, message)
+				print state
 			else: 
-			    print "#Connection to %s closed" % ip 
-			    sock.close() 
-			    clients.remove(sock) 
+				if ipPortTuple in state.keys() and state[ ipPortTuple ] == "ok":
+					print "authenticated!"
+				else:
+					print "not authenticated"
+			    		print "#Connection to %s closed" % ip 
+			    		sock.close() 
+			    		clients.remove(sock)
+				
+				
+				if message[0:3] == "add":
+			    		print "Adding was requested"
+					
+
+				
+				if message[0:4] == "exit":
+			    		print "#Connection to %s closed" % ip 
+			    		sock.close() 
+			    		clients.remove(sock)
+
+
+
+
+
 	finally: 
 	    for c in clients: 
 		c.close() 
