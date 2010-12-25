1e4aebb71a3db8980cc69e36f8f0cf1340eedc4f
Sebastian Moors <mauser@smoors.de>
2010-12-25 23:51:13 +0100

    webinterface: added monitoring status

--- web/cgi-root/webcore.py
+++ web/cgi-root/webcore.py
@@ -5,7 +5,15 @@ class datenfresser_web:
 		print "Content-Type: text/html"
 		print
 	    	print "<html><head><title>Datenfresser</title><link rel='stylesheet' type='text/css' href='datenfresser.css'></head><body>" ;
-		print " <div id='banner'><img src='images/small.png'><div id='banner_text'><a href='index.py'>Show container</a>&nbsp;<a href='show_logs.py'>Show logs</a>&nbsp;<a href='add_container.py'>Add container</a>&nbsp;</div></div><div height='300'>&nbsp;<br /><br /><br /><br /><br /></div>";
+		print " <div id='banner'><img src='images/small.png'><div id='banner_text'>"
+		print "<a href='index.py'>Show container</a>&nbsp;"
+		print "<a href='show_logs.py'>Show logs</a>&nbsp;"
+
+
+		#print "<a href='add_container.py'>Add container</a>&nbsp;"
+		print "<a href='monitor.py'>View monitoring status</a>&nbsp;"
+		
+		print "</div></div><div height='300'>&nbsp;<br /><br /><br /><br /><br /></div>";
     
     def print_error( self , error_message ):
 		print "<div id='error'>" + error_message + "</div>";
